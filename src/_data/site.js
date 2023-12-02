8ccabe22c7e7569a0ba204cf65472098de07bb09
axambo <anna@Annas-MBP.lan>
2023-12-02 16:42:48 +0000

    fixing disqus new post

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,4 +1,4 @@
 const isDev = process.env.ELEVENTY_ENV === 'development';
 module.exports = {
-  baseUrl: isDev ? 'localhost:8080' : 'https://sensingtheforest.github.io'
+  baseUrl: isDev ? 'http://localhost:8080' : 'https://sensingtheforest.github.io'
 }
