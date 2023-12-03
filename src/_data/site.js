cc5c32fbc773fece2a1288dc5e5002ea36c85b35
axambo <anna@Annas-MBP.lan>
2023-12-03 17:10:02 +0000

    added social: youtube channel

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,4 +1,5 @@
 // process.env.ELEVENTY_ENV seems to not be working, using meta.rootURL instead
+// source: https://github.com/11ty/eleventy/issues/2387
 const isDev = process.env.ELEVENTY_ENV === 'development';
 module.exports = {
   baseUrl: isDev ? 'http://localhost:8080' : 'https://sensingtheforest.github.io'
